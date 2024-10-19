a1ee68cbe145d411af5e9946fead8cd540f46908
Deepak .R <deepakravikumar04@gmail.com>
2024-10-19 20:19:24 +0530

    Create SortingNumbers.py

---
+++ SortingNumbers.py
@@ -0,0 +1,7 @@
+# Sort a list of numbers in ascending and descending order
+numbers = [5, 2, 9, 1, 5, 6]
+numbers.sort()  # Ascending order
+print("Sorted list (ascending):", numbers)
+
+numbers.sort(reverse=True)  # Descending order
+print("Sorted list (descending):", numbers)
