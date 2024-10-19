117484d5da9a91c85e0c6f51b86d92e46915c7da
Deepak .R <deepakravikumar04@gmail.com>
2024-10-19 21:19:38 +0530

    Create GCD.py

---
+++ GCD.py
@@ -0,0 +1,8 @@
+# Calculate the GCD of two numbers entered by the user
+import math
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+gcd = math.gcd(num1, num2)
+print(f"The GCD of {num1} and {num2} is {gcd}.")
